f91ecf7fdd82f821df54f516dc80a3cc4bc576f5
dmitrij6523 <158767012+dmitrij6523@users.noreply.github.com>
2024-09-26 09:09:14 +0300

    Add files via upload

---
+++ Kalkul.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulyator
+{
+    internal class Kalkul
+    {
+
+        double num_one;
+        double num_two;
+
+        public void plus(double num_one, double num_two)
+        {
+            Console.WriteLine(num_one + num_two);
+        }
+        public void minus(double num_one, double num_two)
+        {
+            Console.WriteLine(num_one - num_two);
+        }
+        public void multiplication(double num_one, double num_two)
+        {
+            Console.WriteLine(num_one * num_two);
+        }
+        public void division(double num_one, double num_two)
+        {
+            if (num_two == 0)
+            {
+                Console.WriteLine("На 0 делить нельзя");
+            }
+            else
+            {
+                Console.WriteLine(num_one / num_two);
+            }
+
+        }
+
+        public void rounding(double num_one, double num_two) //округление
+        {
+            Console.WriteLine(Math.Round(num_one));
+            Console.WriteLine(Math.Round(num_two));
+        }
+
+        public void arksin(double num_one, double num_two) //arksin
+        {
+            Console.WriteLine(Math.Atan(num_one));
+        }
+
+        public void degree(double num_one, double num_two) // степень
+        {
+            Console.WriteLine(Math.Pow(num_one, num_two));
+        }
+
+        public void square(double num_one, double num_two) // квадрат
+        {
+            Console.WriteLine(Math.Sqrt(num_one));
+            Console.WriteLine(Math.Sqrt(num_two));
+        }
+        public void sin(double num_one, double num_two) //синус
+        {
+            Console.WriteLine(Math.Sin(num_one));
+            Console.WriteLine(Math.Sin(num_two));
+        }
+        public void tan(double num_one, double num_two)
+        {
+            Console.WriteLine(Math.Tan(num_one));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using Kalkulyator;
+
+start:
+
+Kalkul clas = new Kalkul();
+
+string simb;
+
+Console.WriteLine("Введите первое число");
+double x = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("Введите действие");
+simb = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+double y = Convert.ToDouble(Console.ReadLine());
+
+
+
+switch (simb)
+{
+    case "+":
+        clas.plus(x, y);
+        goto start;
+    case "-":
+        clas.minus(x, y);
+        goto start;
+    case "*":
+        clas.multiplication(x, y);
+        goto start;
+    case "/":
+        clas.division(x, y);
+        goto start;
+
+    case "округление":
+        clas.rounding(x, y);
+        goto start;
+    case "arksin":
+        clas.arksin(x, y);
+        goto start;
+    case "степень":
+        clas.degree(x, y);
+        goto start;
+    case "квадрат":
+        clas.square(x, y);
+        goto start;
+    case "синус":
+        clas.sin(x, y);
+        goto start;
+    case "тангенс":
+        clas.tan(x, y);
+        goto start;
+}
